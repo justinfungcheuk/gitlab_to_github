03fe460112c6b8d567d128c7cf5d031e29de3507
Justin Fung <justinfungcheukhk@gmail.com>
2020-04-26 02:48:09 +0800

    Finished part 2 of exercise 2

--- WEF001/index.js
+++ WEF001/index.js
@@ -1,16 +1,30 @@
 console.log("Hello, World!")
-let sum = 1+1;
+let sum = 1 + 1;
 sum += 1;
 //equals to sum = sum + 1;
 console.log(sum);
 
 var x = 1;
 let y = 2;
-const z =3;
+const z = 3;
 w = 123;
 
-console.log("x + y:" + (x+y));
-console.log(`y - z: ${y-z}`);
-console.log(`z * w: ${z*w}`);
+console.log("x + y:" + (x + y));
+console.log(`y - z: ${y - z}`);
+console.log(`z * w: ${z * w}`);
 console.log(`w / x: ${w / x}`);
-console.log(`w % x: ${w%x}`);
+console.log(`w % x: ${w % x}`);
+
+const PTS = 27.5;
+const REB = 8.5;
+const AST = 8.0;
+const STL = 1.3;
+const BLK = 0.6;
+const missedFG = 19.8 - 10.2;
+const missedFT = 7.7 - 5.1;
+const T0 = 3.5;
+const GP = 51;
+
+const efficiency = (PTS + REB + AST + STL + BLK - missedFG - missedFT - T0) / GP;
+
+console.log("Efficiency of Leborn James is "+efficiency);
