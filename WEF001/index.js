ffd406e94c4c82743d39720584de4bd175b8d881
Justin Fung <justinfungcheukhk@gmail.com>
2020-04-26 02:31:40 +0800

    Finished first part of exercise 2

---
+++ WEF001/index.js
@@ -0,0 +1,16 @@
+console.log("Hello, World!")
+let sum = 1+1;
+sum += 1;
+//equals to sum = sum + 1;
+console.log(sum);
+
+var x = 1;
+let y = 2;
+const z =3;
+w = 123;
+
+console.log("x + y:" + (x+y));
+console.log(`y - z: ${y-z}`);
+console.log(`z * w: ${z*w}`);
+console.log(`w / x: ${w / x}`);
+console.log(`w % x: ${w%x}`);
