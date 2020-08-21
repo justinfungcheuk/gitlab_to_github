fe20db5732a353d5adcf75c9f8a1187379100508
Justin Fung <justinfungcheukhk@gmail.com>
2020-08-22 01:16:25 +0800

    JS Exercises

--- WEF002/01/01/index.js
+++ WEF002/01/01/index.js
@@ -1,37 +1,49 @@
-
 // 1) Generate a random integer from 0 to 10 (reference: Math.random())
 const rand = Math.floor(Math.random() * 11) // Math.floor()執行是向下舍入的,即是0.99...或以下等於0, 1.99...以下等於1, 如此類推
+    // 上面的寫法 (Math.random() * 11) 是 取得 0 至 10 的值
+    // const rand = Math.floor(Math.random() * 10) + 1 是取得 1 至 10 的值
+
+// 所以先寫 const 較為理想, 去進展下一步
 
 // 2) Ask the user input (reference: prompt())
 let input; // <<<-- 該為undefined |||　const input是不可行的, 因為const的事物已經assign了, 是不可以再assign的, 而let則可以重新assign, 所以最好的寫法是先做了const先
-let attempts = 0;
+//因為需要持續使用該 input　所以將 let input 放在 do... while loop的 外面
+//而由於 input 需要再次使用及更改所以就把使用 let input; 而不是使用 const input
+let attempts = 0; // attempts (出手次數)
 do {
     // 6) The game ends and the player loses when the player has guessed it wrong for more than 3 times. 
     attempts++;
-    if (attempts >= 3) {
+    if (attempts > 3) {
         break; //break表示不再運作該項目, 如上面例子: 如果attempts大於或等於3就不再運行項目
     }
 
-// 5) You may hint the player whether the number is larger or smaller
-if (typeof input === 'undefined') { // typeof表示屬於甚麼類型, 表示如何檢查是否undefined, 何謂type?例如: typeof = 1是number | typeof true是boolean | let a;是undefined | typeof a是undefined;
-    input = prompt ('Welcome! Gimme a number, please.')
+    // 5) You may hint the player whether the number is larger or smaller
+    if (typeof input === 'undefined') { // typeof表示屬於甚麼類型, 表示如何檢查是否undefined, 何謂type?例如: typeof = 1是number | typeof true是boolean | let a;是undefined | typeof a是undefined;
+        // undefined 表示沒有任何東西
+        input = prompt('Welcome! Gimme a number, please.')
     } else if (input > rand) {
-    input = prompt ('What is the number? (Should be smaller)');
+        input = prompt('What is the number? (Should be smaller)');
     } else {
-    input = prompt ('What is the number? (Should be larger)');
+        input = prompt('What is the number? (Should be larger)');
 
-/*    attempts++;
-    if (attempts >= 3) {
-        break; //break表示不再運作該項目, 如上面例子: 如果attempts大於或等於3就不再運行項目
+        /*    attempts++;
+            if (attempts >= 3) {
+                break; //break表示不再運作該項目, 如上面例子: 如果attempts大於或等於3就不再運行項目
+            }
+        */
     }
-*/
-    }
-// 3) If the number does not match, go back to step 2
+    // 3) If the number does not match, go back to step 2
 } while (parseInt(input) !== rand); // != 表示不等於的意思 而常用的是 === 及 !==意思是一樣的, 為何常用? 因為較為準確的計算
+// 感嘆號 !== 表示將等於的值對上它的類別 例如: 2 == "2" (這個 "2"是一個string)
 // 上句寫法是表示 string可以等於 integer. 而如何將string及integer比較呢? 是可以將該parseInt由String轉回integer (整數),就可以做出比較
 
 // 4) If the number matches, the player wins.
 if (parseInt(input) === rand) {
+    /* 由於 string 是不可以等於 integer(整數)
+     * 那麼如何能夠將 string 與 integer做比較呢?
+     * 就是可以將它們透過使用 parseInt 由 string 轉換為 integer, 就可以作出比較
+     */
+
     alert('Congratz. The answer is ' + rand);
 } else {
     alert('Sorry, bad luck. The correct answer is ' + rand);
--- WEF002/01/02/index.js
+++ WEF002/01/02/index.js
@@ -11,9 +11,16 @@
 /*
 while (true) {
     const year = prompt("Which year ar?");
-    if (year === '' || year === null) { //表示 '' 一個empty string 
-        break;
+    if (year === '' || year === null) { // '' 表示 一個empty string
+    // year === '' 當中的 '' empty string 代表無任何東西
+    // If the users clicks the Cancel button, this function returns null
+    // 所以要寫上 year === null, 表示如果該 year 是 null 就停止運作
+
+        break; 
+        //  break 代表將該 while loop 停止
     }
+
+
     if (year % 4 === 0) {
         if (year % 100 === 0) {
             if (year % 400 === 0) {
@@ -23,58 +30,77 @@ while (true) {
             }
         } else {
             console.log('Leap year');
+            //不能被 100 整除, 就表示是 leap year
         }
     
     } else {
         console.log('Not a leap year');
-    }
+    } // 有多少個 if 就要有多少個 else, 除非 if當中有 break 來停止運作
 
 }
 */
 while (true) {
     let year = prompt("Which year ar? (e.g. 800, 1988, bc20)");
+    //該部分不使用 const year 而使用 let year 是因為會對 year 作出再處理
     if (year === '' || year === null) { //表示 '' 一個empty string 
         break;
     }
 
-// Regular Expression
-year = year.replace(/[^0-9bc]/g, '') // /[^0-9bc]/g 表示除了0-9bc的資料以外的所有資料都排除在外
+    // Regular Expression 正規表達法 - 將一些不需要的字刪除
+    year = year.replace(/[^0-9bc]/g, '')
+        // /[^0-9bc]/g 表示除了0-9bc的資料以外的所有資料都排除在外
+        /* 使用 replace() 的正規表達法來取代所需要的資料
+         * \s 代表 不要空格
+         * g  代表 多於一個
+         * '' 代表 由空的 string 取代
+         * [^0-9] 將方框包起, 及使用該符號 ^ 就表示除了 0-9 以外的所有東西都會被排除
+         * bc 代表 要bc該字, 但排除其他
+         */
 
-if (year.startsWith('bc')){ // startsWith()表示由bc開始
-    year = - year.substr(2); // 如果只要後面的字就用 substr()表示
-}
+    if (year.startsWith('bc')) { // startsWith()表示由bc開始
+        year = -year.substr(2); // 如果只要後面的字就用 substr()表示
+        // 多數使用 substr(1, 2) 第一個參數 1 代表由那一隻字開始數起, 第二個參數 2 代表提取多少個字
+        // 或者 substr(2) 代表由第二個字開始數起, 將第二個字及後面所有的字提取
+        // year = - year.substr(2).replace(/[^0-9]/g, '') 表示由第二個字開始取代
+    }
 
-// parseInt()這個函數就是在括號裡面輸入一個字符串,然後返回一個整數結果,最簡單的方法是輸入一個字符串,再把字符串裡面的數字給提取出來,得到一個數字類型的數字
-// 但是, 如果是輸入一個 0x開頭的字符串,就會按照十六進制的形式來轉換,如果不設定好規則,很容易發生混款
-// 因此, parseInt可以傳入第二個參數, 第二個參數是一個基數,就是告訴函數需要多少進制
-// 有特殊情況, 第二個參數, 如果參數為0, 那麼就以十進制來轉換, 參數小於2或大於36, 就返回NaN
-year = parseInt(year); // parseInt(string, radix)   將壹個字符串 string 轉換為 radix 進制的整數， radix 為介於2-36之間的數。
-if (isNaN(year)) {
-    console.log('Not a valid year :(');
-    continue; // 不想該運作由此停止, 想該項目繼續運作下一個loop, 可加continue作繼續運算繼續loop
-}
+    // parseInt()這個函數就是在括號裡面輸入一個字符串,然後返回一個整數結果,最簡單的方法是輸入一個字符串,再把字符串裡面的數字給提取出來,得到一個數字類型的數字
+    // 但是, 如果是輸入一個 0x開頭的字符串,就會按照十六進制的形式來轉換,如果不設定好規則,很容易發生混款
+    // 因此, parseInt可以傳入第二個參數, 第二個參數是一個基數,就是告訴函數需要多少進制
+    // 有特殊情況, 第二個參數, 如果參數為0, 那麼就以十進制來轉換, 參數小於2或大於36, 就返回NaN
+    year = parseInt(year); // parseInt(string, radix)   將壹個字符串 string 轉換為 radix 進制的整數， radix 為介於2-36之間的數。
+    if (isNaN(year)) {
+        //如果不是一個數目字, 就透過 continue 繼續輸入資料, 運作下一個 loop
+        console.log('Not a valid year :(');
+        continue; // 不想該運作由此停止, 想該項目繼續運作下一個loop, 可加continue作繼續運算繼續loop
+    }
 
-/* if (year < 1000) {
-    console.log('Why would you bother some year before 1000?');
-    continue;
-}
-*/
+    /* if (year < 1000) {
+        console.log('Why would you bother some year before 1000?');
+        continue;
+    }
+    */
 
-let isLeapYear = false;
-if (year % 4 === 0) {
-    if (year % 100 === 0) {
-        if (year % 400 ===0) {
+    let isLeapYear = false;
+    // 設定該 let isLeapYear = false; 代表不用將其他是 false的值寫出來, 直接設定該值的Boolean為false就可
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            // if year can % 100 === 0, not leap year
+            if (year % 400 === 0) {
+                //exception case, if year % 400 === 0, is leap year
+                isLeapYear = true;
+                // Therefore, if year can % 4 or 100 or 400 === 0, is leap year
+            }
+        } else {
             isLeapYear = true;
-        }
-    } else {
-        isLeapYear = true;
+            // 該 else 代表 year % 400 不是等於 0
         }
     }
 
     if (isLeapYear) {
         console.log(year + ' is a leap year')
     } else {
-        console.log(year + ' is not leap year')
+        console.log(year + " isn\'t a \"leap year\"")
     }
 }
 
