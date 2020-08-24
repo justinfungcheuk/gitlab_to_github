61ee9947add565336f5be34695f6231cf6f1c734
Justin Fung <justinfungcheukhk@gmail.com>
2020-08-25 01:59:53 +0800

    JavaScript-Bind function/Call bound function/block scope variable/Function scope variable/Closure/Function declaration/Function invocation/arrow function/Math Functions/Parsing Function/setTimeout() and setInterval()/Windows Function/Exercise 1: RNA Transcription/Exercise 2:function checkMarkSix

--- WEF003/01/exercise-2.js
+++ WEF003/01/exercise-2.js
@@ -17,47 +17,57 @@ T 	A
 A 	U
 */
 
-function rnaTranscription(dna){
+function rnaTranscription(dna) { // 入來的東西是 dna
     let rna = "";
-    for(let nucleotide of dna){
-        switch(nucleotide){
-            case "G":
-            rna += "C";
-                break;
+    /* 而要砌 RNA, 必須要有一個 result, 該 result 通常在 for loop 的外面, 為甚麼呢?
+     * 因為我在外面 declare, 該 for loop 會先運行, 不會重新 assign 一個新的
+     * 所以當 for loop 運行, 需要該 result, let rna = ""; 去接收所有得出的結果
+     */
+    for (let nucleotide of dna) {
+        switch (nucleotide) {
+            case "G": //第一個版本是 G
+                rna += "C"; //將 G 變成 C
+                break; // 每處理完一個版本, 必須加上 break; 停止該次運作
             case "C":
-            rna += "G";
+                rna += "G"; //將 C 變成 G
                 break;
             case "T":
-                rna += "A";
+                rna += "A"; //將 T 變成 A
                 break;
             case "A":
-                rna += "U";
+                rna += "U"; //將 A 變成 U
                 break
             default:
+                // switch 必須在最後寫上 default, 因為當任何情況都不可行的時候, 就是 Default
                 // console.log("Does not exist!");
-                throw new Error('Nucleotide ${nucleotide} Unknown!') //throw的意思代表 我不懂得處理, 你幫我處理它, 但function它就不會幫你處理
+                throw new Error(`Nucleotide ${nucleotide} Unknown!`)
+                    //throw的意思代表 我不懂得處理, 你幫我處理它, 但function它就不會幫你處理
         }
     }
-    return rna;
+    return rna; //當所有以上的版本都處理完畢, 必須加上 return rna
 }
 
 console.log(rnaTranscription("GCTAATGCTAGTCAGT"));
 //以上為第一種寫法
 
+
 //以下為另一種寫法, 較佳
-function rnaTranscriptionWithObj(dna){
+function rnaTranscriptionWithObj(dna) { //為何是一個 obj 呢? 因為會使用 obj 來運作該編碼的要求
     const mapping = {
-        "G":"C",
-        "C":"G",
-        "T":"A",
-        "A":"U"
+        //以下方式是一個 mapping (影射), 即是將 "G" 變成 "C"
+        "G": "C",
+        "C": "G",
+        "T": "A",
+        "A": "U"
     }
     let rna = "";
-    for(let nucleotide of dna){
-        if(! (nucleotide in mapping)){
-            throw new Error('Nucleotide ${nucleotide} Unknown!')
+    for (let nucleotide of dna) {
+        if (!(nucleotide in mapping)) { //如果 nucleotide 不是在 mapping 入面的就運行下面的編碼 
+            throw new Error(`Nucleotide ${nucleotide} Unknown!`)
         }
         rna += mapping[nucleotide];
+        // 可以通過 mapping by 該物件 nucleotide, 這是加返落去的
+        // 即是將 mapping 拿回相對應的 nucleotide 的東西
     }
     return rna;
 }
--- WEF003/02/index2.js
+++ WEF003/02/index2.js
@@ -9,7 +9,7 @@
 // The function should return true if the bid is contained in the result or otherwise false.
 // The function usage should be as follow:
 
-// No console.log inside
+// // 以下整個 function 都是 No console.log inside
 function checkMarkSix(result, bid) {
     let matched = 0;
     for (let num of result) {
@@ -28,8 +28,8 @@ console.log(checkMarkSix([2, 4, 10, 15, 14, 19], [2, 19])); // returns: true
 
 function checkMarkSixAugmented(result, bid) { //Augmented(增強)
     let matched = 0;
-    for (let bidNumber of bid) {
-        for (let num of result) {
+    for (let bidNumber of bid) { //The second parameter is the bid.
+        for (let num of result) { //The first parameter is the mark six result
             if (num === bidNumber) {
                 matched++
 
@@ -43,33 +43,85 @@ console.log(checkMarkSixAugmented([1, 3, 5, 7, 9, 11], [1, 3])); // returns: tru
 console.log(checkMarkSixAugmented([1, 3, 5, 7, 9, 11], [2, 3])); // returns: false
 console.log(checkMarkSixAugmented([2, 4, 10, 15, 14, 19], [2, 19])); // returns: true
 
-// Bonus 1: Create another function quickPicks() that may quickly pick several bids randomly and check against one single result.
+
+
+
+
+
+
+// Bonus 1: 
+// Create another function quickPicks() that may quickly pick several bids randomly and check against one single result.
 // quickPicks([1, 3, 5, 7, 9, 11], 1); // returns: [{"bid": [1, 3], "result": true}]
 // quickPicks([1, 3, 5, 7, 9, 11], 3); // returns: [{"bid": [2, 4], "result": false}, {"bid": [2, 5], "result": false}, {"bid": [7, 9], "result": true}]
 // The first parameter is the mark six result. The second parameter is the number of bids.
 // The function should return an array of the result object with the bid generated and the result from checkMarkSix().
 
-// No console log inside
+
+
+/* 以下整個 function 都是 No console.log inside
+ * 為甚麼不使用 console.log在內部呢? 因為如果寫在內部, 即是代表了你 assume了處理完後一定會 print 出來
+ * 而該 assume 會使你比較困難地重用你的編碼
+ * 所以, 在最後才用 console.log 更容易處理編碼
+ */
 function quickPicks(result, numberOfBids) {
     const bids = [];
+    // []; 該 array 是甚麼呢? 該 array 是儲存起我們要 generate 的那堆 bid
+    /* 而 bids = []; 最後的結果應該如下: 是一個 array of array 的結果
+     * 所以該 array of array 有兩層都是很正常, 
+     * 因為外面的 array 是 const bids = [];, 內裡的 array 是 const bid = []
+     * 所以可以將內裡的處理中的 array, const bid = [] 把每個塞入去外面的 array, const bids = [];
+     * [
+     *    [1,3],
+     *    [2,3],
+     *    [2,19]
+     * ]
+     */
     for (let i = 0; i < numberOfBids; i++) {
         const bid = []
-        while (bid.length < 2) { // 為甚麼用 while loop? 因為不知道要general多久
+        while (bid.length < 2) { // 為甚麼用 while loop? 因為不知道要generate多久
+            //而使用while loop 是因為要逐個逐個 generate 返 
+            // bid.length < 2 是代表 當它未夠 2 個時, 不要離開
+
             const randomNumber = Math.floor(Math.random() * 20) + 1
-            if (bid.includes(randomNumber)) {
+                // Math.floor(Math.random() * 20) + 1 是提取 1 - 20 的數目字
+
+            if (!bid.includes(randomNumber)) {
+                //includes()代表 如包括了原有的數字就是 true, 如沒有的就是 false
+                //例子: [1,2,3,4,5].includes(2) 就代表 true, 而 includes(-2) 代表 false
+                //因為是 randomNumber, 最在乎的是撞不撞, 因為我們要不撞, 
+
                 bid.push(randomNumber);
+                //!表示如果不包含 randomNumber, 就要 push 入去 randomNumber
             }
         }
         bids.push(bid);
+        //將 result 結果擺回入去, 即是將 bid 該 result 擺回進去 bids
+        //因為題目要求當有 3 個的時候要逐個 call
     }
     const bidResults = [];
-    for (let bid of bids) {
+    /* 因為我們要的結果是 [{"bid": [2, 4], "result": false}, {"bid": [2, 5], "result": false}, {"bid": [7, 9], "result": true}]
+     * 而上面的結果也是一個 result, 也是一個 array 來的
+     * 所以, 我們就又需要一個 array, 即是上面的 const bidResults = [];
+     */
 
+    for (let bid of bids) {
+        //然後逐個逐個 push 入去
+        /* [{"bid": [2, 4], "result": false}, {"bid": [2, 5], "result": false}, {"bid": [7, 9], "result": true}]
+         *  這整個是一個 object  這是一個 result(從 checkMarkSix得出的 value)
+         *   而 "bid": 就是塞入去的物件 [2, 4]
+         * 
+         *   所以我們需要寫出 
+         * const bidResult = {
+         *    "result": checkMarkSix(result, bid),
+         *    "bid": bid
+         * }
+         * bidResult.push(bidResult);
+         */
         const bidResult = {
             "result": checkMarkSix(result, bid),
             "bid": bid
         }
-        bidResult.push(bidResult);
+        bidResults.push(bidResult);
     }
     return bidResults;
 }
@@ -77,19 +129,28 @@ function quickPicks(result, numberOfBids) {
 console.log(quickPicks([1, 3, 5, 7, 9, 11], 1)); // returns: [{"bid": [1, 3], "result": true}]
 console.log(quickPicks([1, 3, 5, 7, 9, 11], 3)); // returns: [{"bid": [2, 4], "result": false}, {"bid": [2, 5], "result": false}, {"bid": [7, 9], "result": true}]
 
+
+
+
+
+
 // Bonus 2
 // Continued with the Quick Pick function, do not return the result immediately. Instead, print the result one by one with one second interval.
 // Example Output:
 
-const quickPicksResult = quickPicks([1, 3, 5, 7, 9, 11], 1);
+const quickPicksResult = quickPicks([1, 3, 5, 7, 9, 11], 3);
 
 const intervalId = setInterval(function() {
-    if (quickPicksResult.length > 0) {
-        const nextResult = quickPicksResult.pop();
-        console.log('Your quick pick: [${nextResult.bid}]');
-        console.log(nextResult.result ? "WIN" : "LOSE"); // ? 的意思代表如 if一樣, 如果該項目是 true 就運行某些東西, 如果是 false 就做其他項目
-    } else { // 在 ? 前面的是一個 evaluate去 Boolean一個東西, 在 ? 後面的 "" 該符號內的就是true, 再後面" "的就是 false
+    if (quickPicksResult.length > 0) { // 代表 quickPicksResult 還有東西的話,
+        // 當有東西時, 就運作以下條件
+        const nextResult = quickPicksResult.pop(); // 取出該result
+        console.log(`Your quick pick: [${nextResult.bid}]`);
+        console.log(nextResult.result ? "WIN" : "LOSE");
+        // ? 前面的就代表是一個 Boolean 的一個物件
+        // ? 後面的意思代表如 if一樣, 如果該項目是 true 就運行某些東西, 如果是 false 就做其他項目
+    } else { // 在 ? 前面的是一個 evaluate去 Boolean一個東西, 在 ? 後面的 "" 該符號內的 "WIN" 就是true, 再後面" " 內的 "LOSE" 就是 false
         clearInterval(intervalId);
+        //代表 quickPickResult 沒有東西, 就停止 intervalId 運作
     }
 
-}, 1000);
+}, 1000); //每隔 1 秒檢查 result 有沒有東西, 有就出 result, 沒有就不出
